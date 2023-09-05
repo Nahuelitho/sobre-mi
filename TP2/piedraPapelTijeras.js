978471ed4770ba0b507c86ed425a8992b645ff99
Nahuelitho <nahuel11905@gmail.com>
2023-09-05 17:53:15 -0300

    agregando actualizaciones al proyecto piedra-papel-tijera

--- TP2/piedraPapelTijeras.js
+++ TP2/piedraPapelTijeras.js
@@ -1,6 +1,16 @@
 function jugar() {
+  var nombreInput = document.getElementById('nombre');
+  var nombre = nombreInput.value;
+  if (nombre.trim() === "") {
+    alert("Por favor, ingrese su nombre antes de jugar.");
+    return;
+  }
+  // El código de juego va aquí
+  let saludo = document.getElementById("saludo");
+  saludo.innerHTML="¡Hola, " + nombre + "! Comenzemos el juego.";
+
   let option = document.getElementById("select").value;
-  document.getElementById("optionJugador").innerHTML = "La opcion del Usuario es: " + option ;
+  document.getElementById("optionJugador").innerHTML = "La opcion del Usuario es: " + option;
   //OPCION DE LA PC
   var numDecimal = Math.random() * 3;
   var optionPC = Math.floor(numDecimal);
@@ -10,15 +20,13 @@ function jugar() {
   const tijeras = "Tijera";
 
   let resultPC;
-  if(optionPC===0){
+  if (optionPC === 0) {
     resultPC = "Piedra";
-  }else if(optionPC===1){
+  } else if (optionPC === 1) {
     resultPC = "Papel";
-  }else{
+  } else {
     resultPC = "Tijera";
   }
-
-
 
   if (optionPC === 0) {
     document.getElementById("optionPc").innerHTML = "La opcion de la PC es: " + resultPC;
@@ -34,7 +42,12 @@ function jugar() {
     document.getElementById("result").innerHTML = "Gana la Computadora";
   } else {
     document.getElementById("result").innerHTML = "Gana el usuario";
-};
+  };
 
 }
+// Función para seleccionar una opción en el select según la imagen clicada
+function seleccionar(opcion) {
+  document.getElementById('select').value = opcion;
+}
+
 
