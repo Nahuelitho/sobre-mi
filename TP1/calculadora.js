4ae5dfc9904efb9aa9a10a27301d95b188a09618
Nahuelitho <nahuel11905@gmail.com>
2023-09-23 11:32:50 -0300

    Actualizacion de metodos

--- TP1/calculadora.js
+++ TP1/calculadora.js
@@ -1,37 +1,55 @@
 
-    const resultado = document.getElementById("resultado");
+const resultado = document.getElementById("resultado");
+function reset() {
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+    resultado.innerHTML = "";
+}
+function suma(num1, num2) {
 
-    function calcular() {
-        const option = document.getElementById("operacion").value;
-        const num1 = parseFloat(document.getElementById("numero1").value);
-        const num2 = parseFloat(document.getElementById("numero2").value);
+    return num1 + num2;
+}
+function resta(num1, num2) {
 
-        if (isNaN(num1) || isNaN(num2)) {
-            resultado.innerHTML = "Ingrese números válidos";
-            return;
-        }
+    return num1 - num2;
+}
+function multiplicacion(num1, num2) {
 
-        switch(option) {
-            case "suma":
-                resultado.innerHTML = "Su resultado de la Suma es: "+ num1 + num2;
-                break;
-            case "resta":
-                resultado.innerHTML ="Su resultado de la Resta es: "+ num1 - num2;
-                break;
-            case "division":
-                if (num2 === 0) {
-                    resultado.innerHTML = "No se puede dividir por cero";
-                } else {
-                    resultado.innerHTML ="Su resultado de la Division es: "+ num1 / num2;
-                }
-                break;
-            case "multiplicacion":
-                resultado.innerHTML ="Su resultado de la multiplicacion es: "+ num1 * num2;
-                break;
-            default:
-                resultado.innerHTML = "Opción inválida";
-                break;
-        }
+    return num1 * num2;
+}
+function division(num1, num2) {
+    if (num1 == 0 || num2 == 0) {
+        alert('no puede dividir por cero');
+    } else {
+        return num1 / num2
+    }
+}
+function calcular() {
+    const option = document.getElementById("operacion").value;
+    const num1 = parseFloat(document.getElementById("numero1").value);
+    const num2 = parseFloat(document.getElementById("numero2").value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado.innerHTML = "Ingrese números válidos";
+        return;
     }
 
-   
+    switch (option) {
+        case "suma":
+            resultado.innerHTML = "Su resultado de la Suma es: " + suma(num1, num2);
+            break;
+        case "resta":
+            resultado.innerHTML = "Su resultado de la Resta es: " + resta(num, num2);
+            break;
+        case "division":
+            resultado.innerHTML = "Su resultado de la Division es: " + division(num1, num2);
+            break;
+        case "multiplicacion":
+            resultado.innerHTML = "Su resultado de la multiplicacion es: " + multiplicacion(num1, num2);
+            break;
+        default:
+            resultado.innerHTML = "Opción inválida";
+            break;
+    }
+}
+
